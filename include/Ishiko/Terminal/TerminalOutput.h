7a6182071d8d9b1f7fcdf340d3b1e237ceb12622
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-07-15 20:16:44 +0100

    Updates (#15)

--- include/Ishiko/Terminal/TerminalOutput.h
+++ include/Ishiko/Terminal/TerminalOutput.h
@@ -1,7 +1,7 @@
 /*
     Copyright (c) 2020 Xavier Leclercq
     Released under the MIT License
-    See https://github.com/Ishiko-cpp/Terminal/blob/master/LICENSE.txt
+    See https://github.com/ishiko-cpp/terminal/blob/main/LICENSE.txt
 */
 
 #ifndef _ISHIKO_TERMINAL_TERMINALOUTPUT_H_
